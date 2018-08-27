e83a64377dd89d94b7462da3e3df879454a30ad2
Ranga Lankathilaka <lankathilakamrp@gmail.com>
2018-08-27 10:04:35 +0530

    logome

--- assets/js/vendor/fade.js
+++ assets/js/vendor/fade.js
@@ -6,3 +6,15 @@ $("#viewbtn").click(function () {
         scrollTop: $("#about").offset().top
     }, 1500);
 });
+logome
+
+
+
+$("#logome").click(function () {
+
+    // $("#about").slideUp()
+
+    $('html, body').animate({
+        scrollTop: $("#hello").offset().top
+    }, 1500);
+});
